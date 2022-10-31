ee3ad46498f513dee5443b442cda2145d0710b6e
tatev-aramyan <tatev.aramyan.fd@gmail.com>
2022-10-31 14:45:06 +0400

    Add func to get star coords projectsion

--- functions.py
+++ functions.py
@@ -12,29 +12,28 @@ from skyfield.api import Star, load, wgs84
 from skyfield.data import hipparcos
 from skyfield.projections import build_stereographic_projection
 
-def get_current_time():
-    return datetime.now()
+
+# def get_current_location(dt):
+#     location = 'Yerevan, Armenia'
+#     # location = input('Enter your location name')
+#     when = '2023-01-01 00:00'
+#     # convert date string into datetime object
+#     # get latitude and longitude of our location
+#     locator = Nominatim(user_agent='myGeocoder')
+#     location = locator.geocode(location)
+#     lat, long = location.latitude, location.longitude
+#     return long, lat
 
 
-def get_current_location(dt):
-    location = 'Yerevan, Armenia'
-    when = '2023-01-01 00:00'
-    # convert date string into datetime object
-    # dt = datetime.strptime(when, '%Y-%m-%d %H:%M')
-    # dt = datetime.now()
-    # get latitude and longitude of our location
-    locator = Nominatim(user_agent='myGeocoder')
-    location = locator.geocode(location)
-    lat, long = location.latitude, location.longitude
-    return long, lat
+def convert_time_to_utc(dt, long, lat):
+    # timezone_str = tzwhere.tzwhere().tzNameAt(40.1776245, 44.5126174)
 
-
-def convert_time_to_utc(dt):
-    timezone_str = tzwhere.tzwhere().tzNameAt(40.1776245, 44.5126174)
+    timezone_str = tzwhere.tzwhere().tzNameAt(lat, long)
     local = timezone(timezone_str)
     local_dt = local.localize(dt, is_dst=None)
     utc_dt = local_dt.astimezone(utc)
-    return  utc_dt
+    print(timezone_str,long,lat)
+    return utc_dt
 
 
 def define_observe_time_from_utc(utc_dt):
@@ -52,3 +51,17 @@ def define_sky_obsrve_point(long, lat,t):
     center_object = Star(ra=ra, dec=dec)
     return center_object
 
+
+def get_projection_our_center(earth, t, center_object):
+    center = earth.at(t).observe(center_object)
+    projection = build_stereographic_projection(center)
+    # field_of_view_degrees = 180.0
+    return projection
+
+
+def calc_star_projection(earth, stars, projection, t):
+    # calculate star positions and project them onto a plain space
+    star_positions = earth.at(t).observe(Star.from_dataframe(stars))
+    stars['x'], stars['y'] = projection(star_positions)
+    # print(stars)
+    return stars
